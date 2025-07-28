a3a6e059dfb9c9579ceb579d598a8db7d9e772a9
chakrachand-infra <chakrachandbaggam48@gmail.com>
2025-07-28 15:41:06 +0530

    Create index.js

---
+++ app/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from CI/CD Node.js App!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = app;
