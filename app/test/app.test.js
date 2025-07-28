d7adaffc3221ed62971ae7604555d9e8b1709e4f
chakrachand-infra <chakrachandbaggam48@gmail.com>
2025-07-28 19:28:13 +0530

    Update app.test.js

--- app/test/app.test.js
+++ app/test/app.test.js
@@ -5,6 +5,6 @@ describe('GET /', () => {
   it('should return Hello message', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toEqual(200);
-    expect(res.text).toContain('Hello from CI/CD Node.js App');
+    expect(res.text).toContain('greetings from Chakrachand');
   });
 });
